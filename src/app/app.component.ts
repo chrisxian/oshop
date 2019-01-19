455f22cfc666275578065dc1c3d7a876f9990320
chrisxian <leixian929@gmail.com>
2019-01-19 14:33:26 +0800

    binding to firestore collection and display on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,32 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+// import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore } from "angularfire2/firestore";
 import { Observable } from 'rxjs';
+import { AppService } from './app.service'
+import { config } from "./app.config";
+import { Task } from './app.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'app';
-  items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('tasks').valueChanges();
+  tasks: Observable<any[]>;
+  constructor(private db: AngularFirestore, private taskService: AppService) {}
+
+  ngOnInit(){
+    this.tasks = this.db.collection(config.collection_endpoint).valueChanges();
+    // .snapshotChanges()
+    // .pipe(
+    //       map(actions=>{
+    //         return actions.map(a=>{
+    //         const data = a.payload.doc.data() as Task;
+    //         const id = a.payload.doc.id;
+    //         return {id, data};
+    //   })
+    // }));
   }
 }
