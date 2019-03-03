d23766bfd763319a0232ad7a612d116e108966df
chrisxian <leixian929@gmail.com>
2019-03-03 16:05:46 +0800

    unsubscribe the observable when component is destroyed.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-
+export class AdminProductsComponent implements OnInit, OnDestroy {
   products: any[];
-  
+  subscription: Subscription;
+
   constructor(private productService: ProductService) {
-    this.productService.getAll().subscribe(products => this.products = products)
+    this.subscription = 
+      this.productService.getAll().subscribe(products => this.products = products)
    }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   filter(query: string){
