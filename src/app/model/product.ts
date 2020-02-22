74ae6e48715d0e96c76544bd446c7e8b5cb75c87
chrisxian <leixian929@gmail.com>
2020-02-22 20:19:33 +0800

    321-filtering-products-by-category

--- src/app/model/product.ts
+++ src/app/model/product.ts
@@ -2,6 +2,6 @@ export interface Product{
     id: string; //document id in the database
     title: string;
     price: number;
-    category: string;
+    category: string;//category id
     imageUrl: string;
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-products',
@@ -9,13 +11,25 @@ import { CategoryService } from '../category.service';
 })
 export class ProductsComponent implements OnInit {
 
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
-  
-  constructor(private productService: ProductService, private categoryService: CategoryService) { 
-    this.products$ = productService.getAll();
+  categoryId: string;
+
+  constructor(route: ActivatedRoute,
+    private productService: ProductService,
+    private categoryService: CategoryService) {
+    productService.getAll().subscribe(products => this.products = products);
     //here do not need to subscribe, the async pipe will also trigger the request.
     this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.categoryId = params.get('category');
+
+      this.filteredProducts = this.categoryId ?
+        this.products.filter(p => p.category === this.categoryId) :
+        this.products;
+    });
   }
 
   ngOnInit() {
