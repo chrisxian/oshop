b206262f17e438c1fe83390d5569a88804c03782
chrisxian <leixian929@gmail.com>
2020-02-23 17:56:42 +0800

    use swithMap to deal with 2 observables.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import { ProductService } from '../product.service';
 import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../model/product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -19,17 +20,22 @@ export class ProductsComponent implements OnInit {
   constructor(route: ActivatedRoute,
     private productService: ProductService,
     private categoryService: CategoryService) {
-    productService.getAll().subscribe(products => this.products = products);
-    //here do not need to subscribe, the async pipe will also trigger the request.
-    this.categories$ = categoryService.getAll();
 
-    route.queryParamMap.subscribe(params => {
-      this.categoryId = params.get('category');
+    productService.getAll()
+      .pipe(switchMap(products => {
+        this.products = products;
+        return route.queryParamMap;
+      }))
+      .subscribe(params => {
+        this.categoryId = params.get('category');
 
-      this.filteredProducts = this.categoryId ?
-        this.products.filter(p => p.category === this.categoryId) :
-        this.products;
-    });
+        this.filteredProducts = this.categoryId ?
+          this.products.filter(p => p.category === this.categoryId) :
+          this.products;
+      });
+
+      this.categories$ = this.categoryService.getAll();
+      //no need to subscribe, the asycn pipe '|' also can unwrap the observable.
   }
 
   ngOnInit() {
