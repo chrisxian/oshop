c5f17059d60d78b51ae88290ecc9278fd890667b
chrisxian <leixian929@gmail.com>
2020-03-21 18:23:01 +0800

    Fix when refresh browser, quantity is not displayed.

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Product } from '../model/product';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { concatMap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit, OnDestroy {
 
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
 
   qty: number;
+  subscription: Subscription;
 
   constructor(private cartService: ShoppingCartService) { }
+
+  ngOnInit(): void {
+    let cartId = localStorage.getItem('cartId');
+    if (cartId) {
+      this.subscription = this.cartService.getCart().subscribe(cart => this.qty = this.getQuantity(cart))
+    }
+  }
 
   addToCart(product: Product) {
     this.cartService.addToCart(this.product)
@@ -32,6 +41,10 @@ export class ProductCardComponent {
     }
     let item = shoppingCart.items?.find(x => x.productId == this.product.id);
     return item ? item.quantity : 0
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
 }
