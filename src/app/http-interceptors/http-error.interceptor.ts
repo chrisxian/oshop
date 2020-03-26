219fb01cd8b50818a161fd1dee59b3577051398e
chrisxian <leixian929@gmail.com>
2020-03-26 17:10:07 +0800

    fix tslint warning.

--- src/app/http-interceptors/http-error.interceptor.ts
+++ src/app/http-interceptors/http-error.interceptor.ts
@@ -13,7 +13,7 @@ export class HttpErrorInterceptor implements HttpInterceptor {
             if ([401, 403].indexOf(err.status) !== -1) {
                 // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
                 this.authenticationService.logout();
-                location.reload(true);
+                location.reload(); // reload the Angular single page application.
             }
 
             const error = err.error.message || err.statusText;
