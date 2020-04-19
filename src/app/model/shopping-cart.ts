fa709b84d41c8e23eea8ce1f6e9b3a1e90bb77f7
chrisxian <leixian929@gmail.com>
2020-04-19 08:09:27 +0800

    fix: items of a shopping-cart might be undefined for the first time when created.

--- src/app/model/shopping-cart.ts
+++ src/app/model/shopping-cart.ts
@@ -8,7 +8,7 @@ export class ShoppingCart {
     constructor(public id: string,
         public dateCreated: string,
         public itemsArray: { product: Product, quantity: number }[]) {
-        itemsArray.forEach(x =>
+        itemsArray?.forEach(x =>
             this.items.push(new ShoppingCartItem(x.product, x.quantity)));
     }
 
