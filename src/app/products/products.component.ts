970a46213ecca2acec90d6e097419a9c4d7712bc
chrisxian <leixian929@gmail.com>
2020-05-05 07:13:19 +0800

    refactor products-component(extract method to let code more readable.)

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -16,24 +16,29 @@ export class ProductsComponent implements OnInit {
 
   categoryId: string;
 
-  constructor(route: ActivatedRoute,
+  constructor(private route: ActivatedRoute,
     private productService: ProductService) {
-
-    productService.getAll()
-      .pipe(switchMap(products => {
-        this.products = products;
-        return route.queryParamMap;
-      }))
-      .subscribe(params => {
-        this.categoryId = params.get('category');
-
-        this.filteredProducts = this.categoryId ?
-          this.products.filter(p => p.category === this.categoryId) :
-          this.products;
-      });
   }
 
   ngOnInit() {
+    this.populateProducts();
   }
 
+  private populateProducts() {
+    this.productService.getAll()
+      .pipe(switchMap(products => {
+        this.products = products;
+        return this.route.queryParamMap;
+      }))
+      .subscribe(params => {
+        this.categoryId = params.get('category');
+        this.applyFilter();
+      });
+  }
+
+  private applyFilter() {
+    this.filteredProducts = this.categoryId ?
+      this.products.filter(p => p.category === this.categoryId) :
+      this.products;
+  }
 }
